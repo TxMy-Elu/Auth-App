c6036a041993ede4ba7798c60acfd4100aff4347
TxMy <to.doguet@gmail.com>
2024-12-09 16:48:42 +0100

    Update baseSql.sql

--- baseSql.sql
+++ baseSql.sql
@@ -23,23 +23,22 @@ CREATE TABLE log (
 ) ENGINE=InnoDB;
 
 -- Création de la table reactivation
-ALTER TABLE log ADD FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(idUtilisateur);
-CREATE TABLE reactivation(
-                             idReactivation INT AUTO_INCREMENT NOT NULL,
-                             codeReactivation VARCHAR(32) NOT NULL,
-                             dateHeureExpirationReactivation DATETIME NOT NULL DEFAULT (NOW() + INTERVAL 1 DAY),
-                             idUtilisateur INT NOT NULL,
-                             PRIMARY KEY(idReactivation),
-                             FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(idUtilisateur)
-)ENGINE=InnoDB;
+CREATE TABLE reactivation (
+                              idReactivation INT AUTO_INCREMENT NOT NULL,
+                              codeReactivation VARCHAR(32) NOT NULL,
+                              dateHeureExpirationReactivation DATETIME NOT NULL DEFAULT (NOW() + INTERVAL 1 DAY),
+                              idUtilisateur INT NOT NULL,
+                              PRIMARY KEY (idReactivation),
+                              FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(idUtilisateur)
+) ENGINE=InnoDB;
 
--- Création de la table reset_password
-CREATE TABLE reset_password (
-                                idResetPassword INT AUTO_INCREMENT NOT NULL,
-                                codeResetPassword VARCHAR(32) NOT NULL,
-                                idUtilisateur INT NOT NULL,
-                                dateHeureExpirationResetPassword DATETIME DEFAULT (CURRENT_TIMESTAMP + INTERVAL 1 DAY),
-                                PRIMARY KEY (idResetPassword),
-                                KEY (idUtilisateur),
-                                FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(idUtilisateur)
+-- Création de la table recuperation
+CREATE TABLE recuperation (
+                              idRecuperation INT AUTO_INCREMENT NOT NULL,
+                              codeRecuperation VARCHAR(32) NOT NULL,
+                              idUtilisateur INT NOT NULL,
+                              dateHeureExpirationRecuperation DATETIME DEFAULT (CURRENT_TIMESTAMP + INTERVAL 1 DAY),
+                              PRIMARY KEY (idRecuperation),
+                              KEY (idUtilisateur),
+                              FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(idUtilisateur)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
